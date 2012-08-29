b609ac32f6cebcfaec5ed7e9f8387ae247abfc0f
andresviikmaa <andres.viikmaa@gmail.com>
2012-08-29 22:30:50 +0300

    inital version (concept testing)

---
+++ game/assets/zputnik.json.js
@@ -0,0 +1,218 @@
+goog.provide('lime.ASSETS.zputnik.json');
+goog.require('soy');
+
+lime.ASSETS.zputnik.json.data = function(opt_data) { 
+return {"frames": {
+
+"zputnik-idle-0.png":
+{
+	"frame": {"x":2,"y":2,"w":75,"h":96},
+	"rotated": true,
+	"trimmed": true,
+	"spriteSourceSize": {"x":45,"y":9,"w":75,"h":96},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-idle-1.png":
+{
+	"frame": {"x":98,"y":79,"w":77,"h":94},
+	"rotated": true,
+	"trimmed": true,
+	"spriteSourceSize": {"x":43,"y":11,"w":77,"h":94},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-idle-10.png":
+{
+	"frame": {"x":192,"y":2,"w":75,"h":94},
+	"rotated": true,
+	"trimmed": true,
+	"spriteSourceSize": {"x":45,"y":10,"w":75,"h":94},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-idle-2.png":
+{
+	"frame": {"x":286,"y":79,"w":79,"h":92},
+	"rotated": true,
+	"trimmed": true,
+	"spriteSourceSize": {"x":44,"y":13,"w":79,"h":92},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-idle-3.png":
+{
+	"frame": {"x":2,"y":156,"w":85,"h":92},
+	"rotated": true,
+	"trimmed": true,
+	"spriteSourceSize": {"x":44,"y":12,"w":85,"h":92},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-idle-4.png":
+{
+	"frame": {"x":380,"y":79,"w":83,"h":90},
+	"rotated": true,
+	"trimmed": true,
+	"spriteSourceSize": {"x":44,"y":14,"w":83,"h":90},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-idle-5.png":
+{
+	"frame": {"x":194,"y":79,"w":77,"h":90},
+	"rotated": true,
+	"trimmed": true,
+	"spriteSourceSize": {"x":45,"y":14,"w":77,"h":90},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-idle-6.png":
+{
+	"frame": {"x":100,"y":2,"w":75,"h":90},
+	"rotated": true,
+	"trimmed": true,
+	"spriteSourceSize": {"x":45,"y":14,"w":75,"h":90},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-idle-7.png":
+{
+	"frame": {"x":2,"y":79,"w":75,"h":94},
+	"rotated": true,
+	"trimmed": true,
+	"spriteSourceSize": {"x":45,"y":10,"w":75,"h":94},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-idle-8.png":
+{
+	"frame": {"x":288,"y":2,"w":75,"h":92},
+	"rotated": true,
+	"trimmed": true,
+	"spriteSourceSize": {"x":45,"y":11,"w":75,"h":92},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-idle-9.png":
+{
+	"frame": {"x":382,"y":2,"w":75,"h":94},
+	"rotated": true,
+	"trimmed": true,
+	"spriteSourceSize": {"x":45,"y":9,"w":75,"h":94},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-running-0.png":
+{
+	"frame": {"x":232,"y":476,"w":113,"h":102},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":8,"y":12,"w":113,"h":102},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-running-1.png":
+{
+	"frame": {"x":117,"y":472,"w":113,"h":102},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":8,"y":12,"w":113,"h":102},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-running-10.png":
+{
+	"frame": {"x":2,"y":470,"w":113,"h":102},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":8,"y":12,"w":113,"h":102},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-running-11.png":
+{
+	"frame": {"x":2,"y":366,"w":113,"h":102},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":8,"y":12,"w":113,"h":102},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-running-12.png":
+{
+	"frame": {"x":211,"y":160,"w":113,"h":102},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":8,"y":12,"w":113,"h":102},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-running-13.png":
+{
+	"frame": {"x":96,"y":158,"w":113,"h":102},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":8,"y":12,"w":113,"h":102},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-running-2.png":
+{
+	"frame": {"x":2,"y":262,"w":115,"h":102},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":8,"y":12,"w":115,"h":102},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-running-3.png":
+{
+	"frame": {"x":326,"y":164,"w":115,"h":102},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":8,"y":12,"w":115,"h":102},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-running-4.png":
+{
+	"frame": {"x":246,"y":372,"w":125,"h":102},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":7,"y":12,"w":125,"h":102},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-running-5.png":
+{
+	"frame": {"x":373,"y":268,"w":125,"h":102},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":8,"y":12,"w":125,"h":102},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-running-6.png":
+{
+	"frame": {"x":246,"y":268,"w":125,"h":102},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":8,"y":12,"w":125,"h":102},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-running-7.png":
+{
+	"frame": {"x":117,"y":368,"w":127,"h":102},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":7,"y":12,"w":127,"h":102},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-running-8.png":
+{
+	"frame": {"x":119,"y":264,"w":125,"h":102},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":7,"y":12,"w":125,"h":102},
+	"sourceSize": {"w":165,"h":120}
+},
+"zputnik-running-9.png":
+{
+	"frame": {"x":373,"y":372,"w":123,"h":102},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":7,"y":12,"w":123,"h":102},
+	"sourceSize": {"w":165,"h":120}
+}},
+"meta": {
+	"app": "http://www.texturepacker.com",
+	"version": "1.0",
+	"image": "zputnik.png",
+	"format": "RGBA8888",
+	"size": {"w":500,"h":580},
+	"scale": "0.3",
+	"smartupdate": "$TexturePacker:SmartUpdate:e5df4f12c1d00e7c184775945230e2ad$"
+}
+}
+;
+}
---
+++ game/game.js
@@ -0,0 +1,161 @@
+//set main namespace
+goog.provide('game');
+
+
+//get requirements
+goog.require('lime.Director');
+goog.require('lime.Scene');
+goog.require('lime.Layer');
+goog.require('lime.Circle');
+goog.require('lime.Label');
+goog.require('lime.animation.Spawn');
+goog.require('lime.animation.FadeTo');
+goog.require('lime.animation.ScaleTo');
+goog.require('lime.animation.MoveTo');
+
+goog.require('lime.Sprite');
+goog.require('lime.fill.Frame');
+goog.require('lime.animation.KeyframeAnimation');
+goog.require('lime.SpriteSheet');
+goog.require('lime.SpriteSheet');
+goog.require('lime.parser.JSON');
+goog.require('lime.ASSETS.zputnik.json')
+
+// entrypoint
+game.start = function(){
+
+	game.state = {
+		IDLE: 0,
+		RUN_LEFT: 1,
+		RUN_RIGHT: 2,
+		SHOOT: 3
+	}
+	var move = 0;
+	var director = new lime.Director(document.body,800, 600),
+	    scene = new lime.Scene(),
+	    //viewport = new lime.Sprite().setSize(520, 440).setFill(34,150,0),
+ 		floor = new lime.Sprite().setFill("assets/beforeandnow.jpg").setScale(1.2).setAnchorPoint(0,0.8).setPosition(0,600);
+		/*
+	    target = new lime.Layer().setPosition(512,384),
+        circle = new lime.Circle().setSize(150,150).setFill(255,150,0),
+        lbl = new lime.Label().setSize(160,50).setFontSize(30).setText('TOUCH ME!'),
+        title = new lime.Label().setSize(800,70).setFontSize(60).setText('Now move me around!')
+            .setOpacity(0).setPosition(512,80).setFontColor('#999').setFill(200,100,0,.1);
+*/
+			
+	game.ss = new lime.SpriteSheet('assets/zputnik.png',lime.ASSETS.zputnik.json, lime.parser.JSON);
+			
+	var zputnik = new lime.Sprite().setPosition(80,440)
+        .setFill(game.ss.getFrame('zputnik-idle-0.png'));
+
+	game.anims = [];
+		
+		// keyframe animation
+		game.anims[game.state.IDLE] = new lime.animation.KeyframeAnimation();
+		game.anims[game.state.IDLE].delay= 1/16; // 1/16 sec between frames, too fast otherwise
+		for(var i=0;i<11;i++){ //add the frames
+			if (i == 7) continue;
+			game.anims[game.state.IDLE].addFrame(game.ss.getFrame('zputnik-idle-'+i+'.png'));
+		}
+		// keyframe animation
+		game.anims[game.state.RUN_LEFT] = new lime.animation.KeyframeAnimation();
+		game.anims[game.state.RUN_LEFT].delay= 1/25; // 1/16 sec between frames, too fast otherwise
+		for(var i=0;i<14;i++){ //add the frames
+			if (i == 5) continue;
+			game.anims[game.state.RUN_LEFT].addFrame(game.ss.getFrame('zputnik-running-'+i+'.png'));
+		}
+		// keyframe animation
+		game.anims[game.state.RUN_RIGHT] = new lime.animation.KeyframeAnimation();
+		game.anims[game.state.RUN_RIGHT].delay= 1/25; // 1/16 sec between frames, too fast otherwise
+		for(var i=0;i<14;i++){ //add the frames
+			if (i == 5) continue;
+			game.anims[game.state.RUN_RIGHT].addFrame(game.ss.getFrame('zputnik-running-'+i+'.png'));
+		}
+		
+		zputnik.runAction(game.anims[game.state.IDLE]);
+
+
+    //add target and title to the scene
+    scene.appendChild(floor);
+
+    scene.appendChild(zputnik);
+
+	director.makeMobileWebAppCapable();
+
+	game.lastState = game.state.IDLE;
+	game.newState = game.state.IDLE;
+	goog.events.listen(director, ['keydown'], function(e) {
+		switch(e.event.keyCode)
+		{
+		case 39:
+		    console.log(1);
+			game.newState = game.state.RUN_LEFT
+			break;
+		case 37:
+			game.newState = game.state.RUN_RIGHT;
+			break;
+		}
+		
+		e.swallow(['keyup'],function(){
+			game.newState = game.state.IDLE;
+        });
+		
+	});
+
+
+	// set current scene active
+	director.replaceScene(scene);
+
+	var velocity = 0.3;
+	lime.scheduleManager.schedule(function(dt){
+	
+		var updateZputnik = false;
+		var position = zputnik.getPosition();
+		var position2 = floor.getPosition();
+		if (game.newState == game.state.RUN_LEFT) {
+			position.x += velocity * dt;	// if dt is bigger we just move more
+			if (position.x > 390 && position2.x > -370) {
+				position.x = 390;
+			};
+			if (position.x == 390) {
+				position2.x -= velocity * dt;
+				if (position2.x < -370) {
+					position2.x = -370;
+				}
+			}
+			if (position.x > 720) {
+				position.x = 720;
+				game.newState = game.state.IDLE;
+			};			
+			//console.log(position.x, position2.x);
+		} else if (game.newState == game.state.RUN_RIGHT) {
+			position.x -= velocity * dt;	// if dt is bigger we just move more
+			if (position.x < 80) {
+				game.newState = game.state.IDLE;
+				position.x = 80;
+			};
+			if (position.x < 390 && position2.x < 0) {
+				position.x = 390;
+			};
+
+			if (position.x == 390) {
+				position2.x += velocity * dt;
+				if (position2.x > 0) {
+					position2.x = 0;
+				}
+			}			
+		}
+		
+		if (game.lastState != game.newState) {
+			zputnik.runAction(game.anims[game.newState]);
+			game.lastState = game.newState;
+		}
+
+		zputnik.setPosition(position); 		
+		floor.setPosition(position2); 		
+	}, this);	
+}
+
+
+//this is required for outside access after code is compiled in ADVANCED_COMPILATIONS mode
+goog.exportSymbol('game.start', game.start);
